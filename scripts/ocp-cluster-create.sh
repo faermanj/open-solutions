9238187b7e4b0fe38ce7f3026d06ca1433045237
Julio Faerman <356476+faermanj@users.noreply.github.com>
2025-03-14 15:05:56 -0300

    update .gitignore and devbox.lock; modify ocp-cluster-create.sh for improved data directory handling and add devbox-init-hook.sh script

--- scripts/ocp-cluster-create.sh
+++ scripts/ocp-cluster-create.sh
@@ -2,12 +2,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 REPO_DIR=$(dirname $DIR)
 
-set -x
-
-echo "Executing test case [$CLUSTER_NAME]"
-
 export CASE_NAME=${CASE_NAME:-"ocp-lab"}
-export DATA_DIR="$REPO_DIR/.data"
+export DATA_DIR="$REPO_DIR/tmp"
 
 export AWS_REGION=${AWS_REGION:-"us-east-1"}
 export CLUSTER_NAME="${CLUSTER_NAME:-"$CASE_NAME-$(date +%H%M)"}"
@@ -52,13 +48,10 @@ fi
 echo "Case [$CLUSTER_NAME][$(date)] cluster created."
 echo "Creation time: $execution_time_minutes minutes"
 
-# backup default kube config if exists with a timestamp
-if [ -f $HOME/.kube/config ]; then
-    cp $HOME/.kube/config $HOME/.kube/config.$(date +%s)
-fi
-
 # replace default kube config
-cp $CLUSTER_DIR/auth/kubeconfig $KUBECONFIG $HOME/.kube/config
+# cp $CLUSTER_DIR/auth/kubeconfig $KUBECONFIG $HOME/.kube/config
+echo "export KUBECONFIG=$CLUSTER_DIR/auth/kubeconfig" 
+export KUBECONFIG=$CLUSTER_DIR/auth/kubeconfig
 
 # Check status
 $CLUSTER_DIR/bin/oc status | tee $CLUSTER_DIR/log/oc-status.log.txt
@@ -71,24 +64,24 @@ if [ -f "$CLUSTER_DIR/case-main.sh" ]; then
     source "$CLUSTER_DIR/case-main.sh" | tee $CLUSTER_DIR/log/case-main.log.txt
 fi
 
-echo "Case [$CLUSTER_NAME][$(date)] collecting must gather..."
-$CLUSTER_DIR/bin/oc adm must-gather | tee $CLUSTER_DIR/log/must-gather.log.txt
-mv must-gather* $CLUSTER_DIR/log/
+# echo "Case [$CLUSTER_NAME][$(date)] collecting must gather..."
+# $CLUSTER_DIR/bin/oc adm must-gather | tee $CLUSTER_DIR/log/must-gather.log.txt
+# mv must-gather* $CLUSTER_DIR/log/
 
-if [ -f "$CLUSTER_DIR/lab.cluster.retain" ]; then
-    info "Retaining cluster [$CLUSTER_NAME]"
-    info "When ready to dispose, use the following command"
-    info "$CLUSTER_DIR/bin/openshift-install destroy cluster --dir=$CLUSTER_DIR"
-else
-    info "Deleting cluster [$CLUSTER_NAME]"
-    $CLUSTER_DIR/bin/openshift-install destroy cluster --dir=$CLUSTER_DIR
-fi
+# if [ -f "$CLUSTER_DIR/lab.cluster.retain" ]; then
+#    info "Retaining cluster [$CLUSTER_NAME]"
+#    info "When ready to dispose, use the following command"
+#    info "$CLUSTER_DIR/bin/openshift-install destroy cluster --dir=$CLUSTER_DIR"
+#else
+#    info "Deleting cluster [$CLUSTER_NAME]"
+#    $CLUSTER_DIR/bin/openshift-install destroy cluster --dir=$CLUSTER_DIR
+#fi
 
-echo "Case [$CLUSTER_NAME] considering pruning..."
-if [ -f "$CLUSTER_DIR/case-prune.sh" ]; then
-    echo "Executing prune case hook [$CLUSTER_DIR/case-prune.sh]"
-    source "$CLUSTER_DIR/case-prune.sh" | tee $CLUSTER_DIR/log/case-prune.log.txt
-fi
+#echo "Case [$CLUSTER_NAME] considering pruning..."
+#if [ -f "$CLUSTER_DIR/case-prune.sh" ]; then
+#    echo "Executing prune case hook [$CLUSTER_DIR/case-prune.sh]"
+#    source "$CLUSTER_DIR/case-prune.sh" | tee $CLUSTER_DIR/log/case-prune.log.txt
+#fi
 
 
-echo "Case [$CLUSTER_NAME] done!"
+#echo "Case [$CLUSTER_NAME] done!"
