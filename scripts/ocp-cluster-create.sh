319a957f513379f14c12016eefb1dfd5c7babbd2
Julio Faerman <356476+faermanj@users.noreply.github.com>
2025-03-14 16:27:27 -0300

    wip

--- scripts/ocp-cluster-create.sh
+++ scripts/ocp-cluster-create.sh
@@ -6,11 +6,18 @@ export CASE_NAME=${CASE_NAME:-"ocp-lab"}
 export DATA_DIR="$REPO_DIR/tmp"
 
 export AWS_REGION=${AWS_REGION:-"us-east-1"}
-export CLUSTER_NAME="${CLUSTER_NAME:-"$CASE_NAME-$(date +%H%M)"}"
 export CLUSTER_DIR="$DATA_DIR/$CLUSTER_NAME"
 export SSH_KEY=$(cat $HOME/.ssh/id_rsa.pub)
+export PULL_SECRET=${PULL_SECRET:-$(cat "$HOME/.openshift/pull-secret-latest.json")}
 
-OCP_CONFIG=${OCP_CONFIG:-"default"}
+export OCP_CONFIG=${OCP_CONFIG:-"default"}
+export CLUSTER_NAME="${CLUSTER_NAME:-"$CASE_NAME-$(date +%H%M)-$OCP_CONFIG"}"
+export AWS_CP_INSTANCE_TYPE=${AWS_CP_INSTANCE_TYPE:-"r7i.xlarge"}
+export AWS_CP_INSTANCE_ARCH=${AWS_CP_INSTANCE_ARCH:-"amd64"}
+export AWS_WK_INSTANCE_TYPE=${AWS_CP_INSTANCE_TYPE:-"r7i.xlarge"}
+export AWS_WK_INSTANCE_ARCH=${AWS_CP_INSTANCE_ARCH:-"amd64"}
+
+
 CONFIG_DIR="$REPO_DIR/ocp/$OCP_CONFIG"
 
 echo "Generating cluster [$CLUSTER_NAME] configuration [$OCP_CONFIG]"
@@ -23,13 +30,18 @@ cp $CLUSTER_DIR/install-config.yaml $CLUSTER_DIR/install-config.bak.yaml
 echo "OpenShift version check"
 openshift-install version | tee $CLUSTER_DIR/log/openshift-version.log.txt
 
+echo "Auto-destroy script [$CLUSTER_DIR/destroy.sh]"
+echo "openshift-install destroy cluster --dir=$CLUSTER_DIR" > $CLUSTER_DIR/destroy.sh
+chmod +x $CLUSTER_DIR/destroy.sh
 
-echo "Creating cluster [$CLUSTER_NAME]..."
+echo "Creating cluster [$CLUSTER_NAME]..." | tee $CLUSTER_DIR/log/cluster-name.log.txt
+aws sts get-caller-identity | tee $CLUSTER_DIR/log/aws-identity.log.txt
 sleep 15
 
 start_time=$(date +%s)
-echo openshift-install create cluster --dir=$CLUSTER_DIR | tee $CLUSTER_DIR/log/create-cluster.log.txt
+openshift-install create cluster --dir=$CLUSTER_DIR | tee $CLUSTER_DIR/log/create-cluster.log.txt
 end_time=$(date +%s)
+
 execution_time=$((end_time - start_time))
 execution_time_minutes=$(echo "scale=2; $execution_time / 60" | bc)
 
@@ -56,13 +68,13 @@ export KUBECONFIG=$CLUSTER_DIR/auth/kubeconfig
 # Check status
 $CLUSTER_DIR/bin/oc status | tee $CLUSTER_DIR/log/oc-status.log.txt
 
-echo "Executing test..."
-sleep 15
+# echo "Executing test..."
+# sleep 15
 
-if [ -f "$CLUSTER_DIR/case-main.sh" ]; then
-    echo "Executing main case hook [$CLUSTER_DIR/case-main.sh]"
-    source "$CLUSTER_DIR/case-main.sh" | tee $CLUSTER_DIR/log/case-main.log.txt
-fi
+# if [ -f "$CLUSTER_DIR/case-main.sh" ]; then
+#     echo "Executing main case hook [$CLUSTER_DIR/case-main.sh]"
+#     source "$CLUSTER_DIR/case-main.sh" | tee $CLUSTER_DIR/log/case-main.log.txt
+# fi
 
 # echo "Case [$CLUSTER_NAME][$(date)] collecting must gather..."
 # $CLUSTER_DIR/bin/oc adm must-gather | tee $CLUSTER_DIR/log/must-gather.log.txt
@@ -85,3 +97,7 @@ fi
 
 
 #echo "Case [$CLUSTER_NAME] done!"
+echo "export KUBECONFIG=$CLUSTER_DIR/auth/kubeconfig" 
+
+echo "Cluster [$CLUSTER_NAME] created."
+
---
+++ scripts/ocp-gitops-install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SRC_DIR=$(dirname $DIR)
+
+# Define variables
+NAMESPACE="openshift-gitops"
+CONFIG_FOLDER="SRC_DIR/gitops/"
+
+# Create the namespace for OpenShift GitOps
+echo "Creating namespace $NAMESPACE..."
+oc create namespace $NAMESPACE
+
+# Install the OpenShift GitOps operator
+echo "Installing OpenShift GitOps operator..."
+oc apply -f https://raw.githubusercontent.com/redhat-developer/gitops-operator/release-latest/deploy/openshift-gitops/subscription.yaml -n $NAMESPACE
+
+# Wait for the operator to be ready
+echo "Waiting for the OpenShift GitOps operator to be ready..."
+oc wait --for=condition=available --timeout=600s deployment/openshift-gitops-operator -n $NAMESPACE
+
+# Apply the configuration from the specified folder
+echo "Applying configuration from $CONFIG_FOLDER..."
+oc apply -f $CONFIG_FOLDER -n $NAMESPACE
+
+echo "OpenShift GitOps installation and configuration complete."
